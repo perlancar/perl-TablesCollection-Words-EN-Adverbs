fd457c0837a8e04e662c4b5f32a6596243d4c6ac
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-05 09:12:49 +0700

    Initial commit

---
+++ lib/TablesCollection/Words/EN/Adverbs.pm
@@ -0,0 +1,25 @@
+package TablesCollection::Words::EN::Adverbs;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of Tables:: modules that contain English adverbs
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<Tables>:: modules:
+
+# INSERT_MODULES_LIST /^Tables::/
+
+
+=head1 SEE ALSO
+
+L<Tables>
+
+L<TablesCollection::Words::EN::Nouns>,
+L<TablesCollection::Words::EN::Adjectives>.
+
+=cut
